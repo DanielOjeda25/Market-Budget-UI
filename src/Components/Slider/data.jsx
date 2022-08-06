e1da1cca2891d4fc34917b429a92ba51fece95fe
Daniel Alejandro Ojeda <ojedadanielalejandro333@gmail.com>
2022-08-06 14:22:30 -0300

    Se agrego nueva data

--- src/Components/Slider/data.jsx
+++ src/Components/Slider/data.jsx
@@ -6,22 +6,75 @@ export const sliderItems = [
   {
     id: 1,
     img: `${img1}`,
-    title: "SUMMER SALE",
+    title: 'SUMMER SALE',
     desc: "DON'T COMPROMISE ON STYLE! GET FLAT 30% OFF FOR NEW ARRIVALS.",
-    bg: "ffefef",
+    bg: 'ffefef',
   },
   {
     id: 2,
     img: `${img2}`,
-    title: "AUTUMN COLLECTION",
+    title: 'AUTUMN COLLECTION',
     desc: "DON'T COMPROMISE ON STYLE! GET FLAT 30% OFF FOR NEW ARRIVALS.",
-    bg: "fafcc1",
+    bg: 'fafcc1',
   },
   {
     id: 3,
     img: `${img3}`,
-    title: "LOUNGEWEAR LOVE",
+    title: 'LOUNGEWEAR LOVE',
     desc: "DON'T COMPROMISE ON STYLE! GET FLAT 30% OFF FOR NEW ARRIVALS.",
-    bg: "f6f6f6",
-  }
+    bg: 'f6f6f6',
+  },
+]
+
+export const categories = [
+  {
+    id: 1,
+    img: 'https://i.ibb.co/cvpntL1/hats.png',
+    title: 'HATS',
+  },
+  {
+    id: 2,
+    img: 'https://i.ibb.co/px2tCc3/jackets.png',
+    title: 'JACKETS',
+  },
+  {
+    id: 3,
+    img: 'https://i.ibb.co/0jqHpnp/sneakers.png',
+    title: 'SNEAKERS',
+  },
+]
+
+export const popularProducts = [
+  {
+    id: 1,
+    img: 'https://i.ibb.co/XzcwL5s/1.png',
+  },
+  {
+    id: 2,
+    img: 'https://i.ibb.co/dJbG1cT/2.png',
+  },
+  {
+    id: 3,
+    img: 'https://i.ibb.co/cTjF6Nd/3.png',
+  },
+  {
+    id: 4,
+    img: 'https://i.ibb.co/px2tCc3/jackets.png',
+  },
+  {
+    id: 5,
+    img: 'https://i.ibb.co/0jqHpnp/sneakers.png',
+  },
+  {
+    id: 6,
+    img: 'https://i.ibb.co/cvpntL1/hats.png',
+  },
+  {
+    id: 7,
+    img: 'https://i.ibb.co/XzcwL5s/1.png',
+  },
+  {
+    id: 8,
+    img: 'https://i.ibb.co/dJbG1cT/2.png',
+  },
 ]
