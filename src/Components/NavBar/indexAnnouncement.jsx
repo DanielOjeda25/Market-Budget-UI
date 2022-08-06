b11737f9c168606e973596123d9898e69c472890
Daniel Alejandro Ojeda <ojedadanielalejandro333@gmail.com>
2022-08-05 22:20:14 -0300

    "

---
+++ src/Components/NavBar/indexAnnouncement.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import { AnnouncementDiv } from './Navbar.styled'
+
+const Announcement = () => {
+  return (
+    <AnnouncementDiv>50% off discount on our products, limited time</AnnouncementDiv>
+  )
+}
+
+export default Announcement
