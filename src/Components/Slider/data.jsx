abbcb0d999e0dadf8dbbfb3b16b54988d7ee53fc
Daniel Alejandro Ojeda <ojedadanielalejandro333@gmail.com>
2022-08-05 22:20:33 -0300

    data file

---
+++ src/Components/Slider/data.jsx
@@ -0,0 +1,27 @@
+import img1 from '../../assets/1.jpg'
+import img2 from '../../assets/2.jpg'
+import img3 from '../../assets/3.jpg'
+
+export const sliderItems = [
+  {
+    id: 1,
+    img: `${img1}`,
+    title: "SUMMER SALE",
+    desc: "DON'T COMPROMISE ON STYLE! GET FLAT 30% OFF FOR NEW ARRIVALS.",
+    bg: "ffefef",
+  },
+  {
+    id: 2,
+    img: `${img2}`,
+    title: "AUTUMN COLLECTION",
+    desc: "DON'T COMPROMISE ON STYLE! GET FLAT 30% OFF FOR NEW ARRIVALS.",
+    bg: "fafcc1",
+  },
+  {
+    id: 3,
+    img: `${img3}`,
+    title: "LOUNGEWEAR LOVE",
+    desc: "DON'T COMPROMISE ON STYLE! GET FLAT 30% OFF FOR NEW ARRIVALS.",
+    bg: "f6f6f6",
+  }
+]
