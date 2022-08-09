24ae961e270506333e4ea4e37e7ab3c031f5f299
Daniel Alejandro Ojeda <ojedadanielalejandro333@gmail.com>
2022-08-09 17:29:33 -0300

    Nuevo Componente

---
+++ src/Pages/ProductPage.jsx
@@ -0,0 +1,78 @@
+import React from 'react'
+import Navbar from '../Components/NavBar'
+import Announcement from '../Components/NavBar/indexAnnouncement'
+import Newsletter from '../Components/NewsLetter'
+import Footer from '../Components/Footer'
+import img from '../assets/jacket.png'
+import {
+  Container,
+  Wrapper,
+  ImgContainer,
+  Image,
+  InfoContainer,
+  Title,
+  Desc,
+  Price,
+  FilterContainer,
+  Filter,
+  FilterTitle,
+  FilterColor,
+  FilterSize,
+  FilterSizeOption,
+  AddContainer,
+  Amount,
+  AmountContainer,
+  Button
+} from './ProductPage.styled'
+import AddIcon from '@mui/icons-material/Add';
+import RemoveIcon from '@mui/icons-material/Remove';
+
+const ProductPage = () => {
+  return (
+    <Container>
+      <Navbar />
+      <Announcement />
+      <Wrapper>
+        <ImgContainer>
+          <Image src={img} />
+        </ImgContainer>
+        <InfoContainer>
+          <Title>Denim Jumpsuit</Title>
+          <Desc>Lorem</Desc>
+          <Price> $20</Price>
+
+          <FilterContainer>
+            <Filter>
+              <FilterTitle>Color</FilterTitle>
+              <FilterColor color='black' />
+              <FilterColor color='darkblue' />
+              <FilterColor color='gray' />
+            </Filter>
+            <Filter>
+              <FilterTitle>Size</FilterTitle>
+              <FilterSize>
+                <FilterSizeOption>XS</FilterSizeOption>
+                <FilterSizeOption>S</FilterSizeOption>
+                <FilterSizeOption>M</FilterSizeOption>
+                <FilterSizeOption>L</FilterSizeOption>
+                <FilterSizeOption>XL</FilterSizeOption>
+              </FilterSize>
+            </Filter>
+          </FilterContainer>
+          <AddContainer>
+            <AmountContainer>
+              <RemoveIcon />
+              <Amount>1</Amount>
+              <AddIcon />
+            </AmountContainer>
+            <Button>ADD TO CART</Button>
+          </AddContainer>
+        </InfoContainer>
+      </Wrapper>
+      <Newsletter />
+      <Footer />
+    </Container>
+  )
+}
+
+export default ProductPage
